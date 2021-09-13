2ffc6b4e837d35dd05f65978848fe90a3241072e
Park Seng Chul <ochestra365@naver.com>
2021-09-13 10:45:54 +0900

    깔끔한 코딩

--- Tools/LogManager.cs
+++ Tools/LogManager.cs
@@ -8,21 +8,28 @@ namespace BabyCarrot.Tools
     {
         private string _path;
 
+
+        #region Constructors
         public LogManager(string path)
         {
             _path = path;
+            _SetLogPath();
         }
         public LogManager()
+            :this(Path.Combine(Application.Root, "Log"))
         {
-            _path = Path.Combine(Application.Root,"Log");
         }
+        #endregion
 
+        #region Methods
         private void _SetLogPath()
         {
             if (!Directory.Exists(_path))
             {
                 Directory.CreateDirectory(_path);//경로가 존재하지 않으면 경로를 만든다.
             }
+            string logFile = DateTime.Now.ToString("yyyyMMdd") + ".txt";
+            _path = Path.Combine(_path, logFile);
         }
 
         public void Write(string data)
@@ -33,5 +40,6 @@ namespace BabyCarrot.Tools
         {
 
         }
+        #endregion
     }
 }
