1be893345c1a58e5f7f80e946ea18dbccd7cf55a
Park Seng Chul <ochestra365@naver.com>
2021-09-13 10:40:03 +0900

    log dll제작중

---
+++ Tools/LogManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO; 
+namespace BabyCarrot.Tools
+{
+    public class LogManager
+    {
+        private string _path;
+
+        public LogManager(string path)
+        {
+            _path = path;
+        }
+        public LogManager()
+        {
+            _path = Path.Combine(Application.Root,"Log");
+        }
+
+        private void _SetLogPath()
+        {
+            if (!Directory.Exists(_path))
+            {
+                Directory.CreateDirectory(_path);//경로가 존재하지 않으면 경로를 만든다.
+            }
+        }
+
+        public void Write(string data)
+        {
+
+        }
+        public void WriteLine(string data)
+        {
+
+        }
+    }
+}
