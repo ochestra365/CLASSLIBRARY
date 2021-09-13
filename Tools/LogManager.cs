d21032c69854f803c96b5b1717bed48584e3d3ee
Park Seng Chul <ochestra365@naver.com>
2021-09-13 11:00:26 +0900

    로그 라이브러리 잘 작동함

--- Tools/LogManager.cs
+++ Tools/LogManager.cs
@@ -34,11 +34,32 @@ namespace BabyCarrot.Tools
 
         public void Write(string data)
         {
-
+            try//혹시라도 로그 작성 중에 예외발생하면 넘어가게 한다.
+            {
+                using (StreamWriter writer = new StreamWriter(_path, true))// 중괄호 안에서만 유효하고 벗어나면 시스템이 자동으로 닫아주고, 메모리를 해제해준다.
+                {
+                    writer.Write(data);//줄바꿈 없이 계속 쓰는 것.
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            
         }
         public void WriteLine(string data)
         {
-
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(_path, true))
+                {
+                    writer.WriteLine(DateTime.Now.ToString("yyyyMMdd HH:mm:ss\t") + data);
+                }
+                //20210913 10:51
+            }
+            catch (Exception ex)
+            {
+            }
+            
         }
         #endregion
     }
