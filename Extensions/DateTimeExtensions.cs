3568ab8a62cb9c898004b7d0ccc6fb7b3a43f845
Park Seng Chul <ochestra365@naver.com>
2021-09-14 13:48:58 +0900

    DateTimeExtension

---
+++ Extensions/DateTimeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BabyCarrot.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime FirstDateOfMonth(this DateTime date)
+        {
+            return new DateTime(date.Year, date.Month, 1);
+        }
+        public static DateTime LastDateOfMonth(this DateTime date)
+        {
+            return new DateTime(date.Year, date.Month, DateTime.DaysInMonth(date.Year, date.Month));
+        }
+    }
+}
---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BabyCarrot.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNumeric(this string s)
+        {
+            long result;
+            return long.TryParse(s, out result);//만약에 성공했을 경우, long형을 어디에 저장할 것인가?
+        }
+
+        public static bool IsDateTime(this string s)
+        {
+            if (String.IsNullOrEmpty(s))
+            {
+                return false;
+            }
+            else
+            {
+                DateTime result;
+                return DateTime.TryParse(s,out result);
+            }
+        }
+    }
+}
