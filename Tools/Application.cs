e2a5a0e9857e761387d39180c54f970f0c3270dd
Park Seng Chul <ochestra365@naver.com>
2021-09-14 13:24:16 +0900

    prefix_postfix 로그매니저

--- Tools/Application.cs
+++ Tools/Application.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 namespace BabyCarrot.Tools
 {
     public static class Application
--- Tools/LogManager.cs
+++ Tools/LogManager.cs
@@ -1,35 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.IO; 
+using System.IO;
 namespace BabyCarrot.Tools
 {
+    public enum LogType { Daily,Monthly}
+
     public class LogManager
     {
+        //어떤 프로그래밍 수십년에 걸쳐서 돌아간다면 생성시 그룹을 지을 경우 관리나 백업이 편하다.
         private string _path;
 
-
         #region Constructors
-        public LogManager(string path)
+        public LogManager(string path,LogType logType,string prefix,string postfix)
         {
             _path = path;
-            _SetLogPath();
+            _SetLogPath(logType,prefix,postfix);
+        }
+        //확장적으로 생각한다.
+        public LogManager(string prefix,string postfix)
+            :this(Path.Combine(Application.Root,"Log"),LogType.Daily,prefix,postfix)
+        {
+
         }
         public LogManager()
-            :this(Path.Combine(Application.Root, "Log"))
+            :this(Path.Combine(Application.Root, "Log"),LogType.Daily,null,null)
         {
         }
         #endregion
 
         #region Methods
-        private void _SetLogPath()
+        private void _SetLogPath(LogType logType,string prefix,string postfix)
         {
+            string path = String.Empty;
+            string name = String.Empty;
+
+            switch (logType) 
+            {
+                case LogType.Daily:
+                    path = String.Format(@"{0}\{1}\", DateTime.Now.Year, DateTime.Now.ToString("MM"));
+                    name = DateTime.Now.ToString("yyyyMMdd");
+                    break;
+                case LogType.Monthly:
+                    path = String.Format(@"{0}\", DateTime.Now.Year);
+                    name = DateTime.Now.ToString("yyyyMM");
+                    break;
+            }
+
+            _path = Path.Combine(_path, path);
             if (!Directory.Exists(_path))
             {
                 Directory.CreateDirectory(_path);//경로가 존재하지 않으면 경로를 만든다.
             }
-            string logFile = DateTime.Now.ToString("yyyyMMdd") + ".txt";
-            _path = Path.Combine(_path, logFile);
+            if (!String.IsNullOrEmpty(prefix))
+            {
+                name = prefix + name;
+
+            }
+            if (!String.IsNullOrEmpty(postfix))
+            {
+                name =  name+postfix;
+
+            }
+            name += ".txt";
+            _path = Path.Combine(_path, name);
         }
 
         public void Write(string data)
@@ -64,3 +96,4 @@ namespace BabyCarrot.Tools
         #endregion
     }
 }
+//윈도 서비스 프로그램이 한 폴더 돌아가게 되면 현재의 로그 매니저는 한 로그 파일을 공유하게 된다. 그러므로 prefix, postfix를 나눠서 사용한다.
